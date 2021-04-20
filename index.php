1623cabb0e6625a8d65c4283550bed16a367926f
Bofferbrauer <75318450+Bofferbrauer@users.noreply.github.com>
2021-04-20 12:33:37 +0200

    css

--- index.php
+++ index.php
@@ -9,13 +9,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>Document</title>
 </head>
 
 <body>
     <nav>
         <?php
-        require_once "components/nav.html";
+        require_once "components/nav.php";
         ?>
 
 

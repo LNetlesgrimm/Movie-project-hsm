7fed5b9b752b610c1b262dc714039fcc7c5da5a5
Bofferbrauer <75318450+Bofferbrauer@users.noreply.github.com>
2021-04-20 14:09:10 +0200

    up

--- pages_html/register.php
+++ pages_html/register.php
@@ -11,6 +11,14 @@
 
 <body>
 
+    <nav>
+        <?php
+        require_once "components/nav.php";
+        ?>
+
+
+    </nav>
+
     <form action="" method="POST">
         <input type="text" name="firstname" placeholder="enter first name">
         <input type="text" name="lastname" placeholder="enter last name">
@@ -22,29 +30,26 @@
 
     <div id="results"></div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
     <script>
-        $(function () {
-            $('input[type="submit"]').click(function (e) {
+        $(function() {
+            $('input[type="submit"]').click(function(e) {
                 e.preventDefault();
                 $.ajax({
-                    url: '../scripts_php/register.php',
-                    method: 'post',
-                    data: $('form').serialize()
-                })
-                    .done(function (result) {
+                        url: '../scripts_php/register.php',
+                        method: 'post',
+                        data: $('form').serialize()
+                    })
+                    .done(function(result) {
                         $("#results").html(result);
                     })
-                    .fail(function (result) {
+                    .fail(function(result) {
                         console.log('AJAX Failed');
                     });
             });
 
         });
-
-
     </script>
 
 </body>

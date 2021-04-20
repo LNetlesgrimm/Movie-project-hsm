423683888d26ec41b3cf484362e999b6ed30cf67
Bofferbrauer <75318450+Bofferbrauer@users.noreply.github.com>
2021-04-20 12:20:07 +0200

    php

---
+++ components/nav.php
@@ -0,0 +1,11 @@
+<?php
+session_start()
+?>
+<ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Login</a></li>
+    <li><a href="../pages_html/register.html">Register</a></li>
+    <li><a href="">Movies</a></li>
+    <li><a href="">Manage categories</a></li>
+    <li><a href="">Add movie</a></li>
+</ul>

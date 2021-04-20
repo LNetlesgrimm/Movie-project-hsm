cf6b59a8e459dd8b5c77e93028906c7f911f3962
Bofferbrauer <75318450+Bofferbrauer@users.noreply.github.com>
2021-04-20 14:39:46 +0200

    nav

--- catalogue.php
+++ catalogue.php
@@ -1,8 +1,6 @@
-<?php
-    include_once('../components/nav.php');
-?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,22 +8,29 @@
     <title>Movie website</title>
     <link rel="stylesheet" href="../style/style.css">
 </head>
+
 <body>
+    <nav>
+
+        <?php
+        require_once 'components/nav.php';
+        ?>
+    </nav>
     <h1>Catalogue</h1>
     <select name="categories" id="selectCategory">
     </select>
-    <script 
-        src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
     </script>
     <script>
-        $(function () {
-            $.ajax( {
-                url: '../scripts_php/catalogue.php'
-            })
-            .done(function(result) {
-                $('#selectCategory').html(result);
-            })
+        $(function() {
+            $.ajax({
+                    url: '../scripts_php/catalogue.php'
+                })
+                .done(function(result) {
+                    $('#selectCategory').html(result);
+                })
         })
     </script>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -6,17 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 
 <body>
-
-    <?php
-    require_once "../components/nav.php";
-    ?>
-
-
-
+    <nav>
+        <?php
+        require_once "components/nav.php";
+        ?>
     </nav>
 
     <form action="" method="POST">

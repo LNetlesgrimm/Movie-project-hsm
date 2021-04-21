9c2f37722a85e7c1346eedd03c3aa3d5f57d2be9
Bofferbrauer <75318450+Bofferbrauer@users.noreply.github.com>
2021-04-21 16:42:40 +0200

    pages

--- scripts_php/movies.php
+++ scripts_php/movies.php
@@ -1,13 +1,12 @@
 <?php
 include_once('database.php');
 
-$movieQuery = "SELECT * FROM movies LIMIT 8";
+$page = ($_GET['page'] - 1) * 8;
+$movieQuery = "SELECT * FROM movies LIMIT 8 OFFSET " . $page;
 $resultMovie = mysqli_query($conn, $movieQuery);
 $movies = mysqli_fetch_all($resultMovie, MYSQLI_ASSOC);
 
 
 $json_movies = json_encode($movies, JSON_PRETTY_PRINT);
-
-
 
 echo $json_movies;
--- scripts_php/pagination.php
+++ scripts_php/pagination.php
@@ -2,15 +2,6 @@
 include_once('database.php');
 
 $movieQuery = "SELECT * FROM movies LIMIT 8";
-$page = 1;
-if (isset("#next")) {
-    $page = $page + 1;
-}
-if (isset("#next")) {
-    $page = $page - 1;
-    if ($page == 0) {
-        $page = 1;
-    }
-}
-$resultMovie = mysqli_query($conn, $movieQuery);
-$movies = mysqli_fetch_all($resultMovie, MYSQLI_ASSOC);
+
+$page = $_GET["numpage"];
+echo $page;

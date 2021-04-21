779ce587661ab194b0b4a9f571ca9237de20dfdd
Machieu <78408466+Machieu@users.noreply.github.com>
2021-04-21 14:42:39 +0200

    Create modify_movie.php

---
+++ modify_movie.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+
+<form action="" method="POST">
+
+    <input type="text" name="title" placeholder=" movie title" value="<?= $movieDetails[0]['title']; ?>">
+    <input type="text" name="poster" placeholder="Poster's URL" value="<?= $movieDetails[0]['poster']; ?>">
+    <input type="date" name="date_of_release" value="<?= $movieDetails[0]['date_of_release']; ?>">
+    <select name="category" id=""></select>
+    <input type="text" name="movie_link" placeholder=" link to the movie" value="<?= $movieDetails[0]['movie_link']; ?>">
+    <input type="textarea" name="synopsis" placeholder="synopsis" value="<?= $movieDetails[0]['synopsis']; ?>">
+    <input type="submit" name="submit_btn" placeholder="sub">
+
+</form>
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
+</script>
+<script>
+    $(function() {
+        $('input[type="submit"]').click(function(e) {
+            e.preventDefault();
+            $.ajax({
+                    url: 'scripts_php/modif.php',
+                    method: 'post',
+                    data: $('form').serialize()
+                })
+                .done(function(result) {
+                    console.log('AJAX succes');
+                })
+                .fail(function(result) {
+                    console.log('AJAX Failed');
+                });
+        });
+
+    });
+</script>

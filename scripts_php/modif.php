81b1a8f0a1cb103c4dbfff18e5b9fce897a97c83
Machieu <78408466+Machieu@users.noreply.github.com>
2021-04-21 16:09:00 +0200

    modify and selected cat

--- scripts_php/modif.php
+++ scripts_php/modif.php
@@ -4,29 +4,31 @@
 $title = htmlspecialchars(trim($_POST['title']));
 $poster = htmlspecialchars(trim($_POST['poster']));
 $date = htmlspecialchars(trim($_POST['date_of_release']));
-$category = htmlspecialchars(trim($_POST['category']));
+$category = htmlspecialchars(trim($_POST['categories']));
 $link = htmlspecialchars(trim($_POST['movie_link']));
 $synopsis = htmlspecialchars(trim($_POST['synopsis']));
+echo $title;
 // hash
 
 // Connect DB
 include_once('database.php');
-
+$movieId = $_POST['id'];
+echo $movieId;
 //query
-$query = "UPDATE movies
+$query = 'UPDATE movies
         SET 
-        title =$title,
-        poster = $poster,
-        date_of_release = $date,
-        category_id = $category,
-        movie_link=$link,
-        synopsis= $synopsis
-        WHERE id =" . $movieId;
-
+        title ="' . $title . '",
+        poster = "' . $poster . '",
+        date_of_release = "' . $date . '",
+        category_id = "' . $category . '",
+        movie_link= "' . $link . '",
+        synopsis= "' . $synopsis . '"
+        WHERE id =' . $movieId;
+echo $query;
 
 $result = mysqli_query($conn, $query);
 
 if ($result)
-    echo '<p style="color:green">modif successfully!</p>';
+    echo 'OK';
 else
     echo '<p style="color:red">Problem with query</p>';

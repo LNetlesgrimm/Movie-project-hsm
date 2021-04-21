5a9b462b4edeea8567b34e3b75e4e2903dc608fd
LNetlesgrimm <kohler.helene@gmail.com>
2021-04-21 14:54:54 +0200

    playlist update

--- scripts_php/movies.php
+++ scripts_php/movies.php
@@ -5,6 +5,7 @@ $movieQuery = "SELECT * FROM movies LIMIT 10";
 $resultMovie = mysqli_query($conn, $movieQuery);
 $movies = mysqli_fetch_all($resultMovie, MYSQLI_ASSOC);
 
+
 $json_movies = json_encode($movies, JSON_PRETTY_PRINT);
 
 echo $json_movies;

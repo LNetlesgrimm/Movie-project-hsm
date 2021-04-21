e66d1c874d6290237edda14df880caa57e7e83be
Bofferbrauer <75318450+Bofferbrauer@users.noreply.github.com>
2021-04-21 10:45:02 +0200

    cat

--- index.php
+++ index.php
@@ -28,10 +28,13 @@ include_once('scripts_php/database.php');
             Laudantium officiis voluptatem architecto! Itaque cumque modi eligendi veritatis, similique sint obcaecati. Earum, dolorum veniam. Magnam amet nulla natus, accusamus nobis in molestias, obcaecati earum corrupti error deleniti et dolorum.</p>
 
         <form action="" method="post" id="mainsearch">
-            <input type="text" name="find" id="find">
+            <input type="text" placeholder="search by category" name="find" id="find">
             <input type="submit" value="Go!">
-
         </form>
+
+        <div>
+            <p id="categories"></p>
+        </div>
 
         <?php
         $totalMoviesCat = "SELECT c.name, COUNT(*) AS total_movies FROM categories as c INNER JOIN movies as m ON c.id = m.category_id group by m.category_id order by total_movies desc limit 4";
@@ -92,6 +95,26 @@ include_once('scripts_php/database.php');
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script>
+        // Wait for the dom to be ready/loaded before executing javascript
+        $(function() {
+            // Send form datas in the background
+            $.ajax({
+                    url: "select_cat.php",
+                    method: "post",
+                })
+                .done(function(result) {
+                    // If AJAX call worked
+                    $("#categories").html(result);
+                })
+                .fail(function(result) {
+                    // If AJAX failed
+                    console.log("AJAX Failed");
+                });
+        });
+    </script>
+
 </body>
 
 </html>
